e17eb9b1b7f69fb219ddef044c2f1f37a650b17e
rafai77 <rafac11@hotmail.com>
2020-09-18 08:54:15 -0500

    pagina para pedidos

--- src/app/addpedido/addpedido.component.ts
+++ src/app/addpedido/addpedido.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbDateParserFormatter, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/internal/operators/debounceTime';
 import { DatosService } from '../services/datos.service';
 import { LogService } from '../services/log.service';
 
@@ -11,16 +13,27 @@ import { LogService } from '../services/log.service';
 })
 export class AddpedidoComponent implements OnInit {
 
-
+  porductoS=[]
+  r:boolean=true;
+  alert= [{type: 'danger',
+  message: 'This is a danger alert',}]
   productos:[]
 
+  private _success = new Subject<string>();
   constructor( public formatter: NgbDateParserFormatter,
     private logiS: LogService, private datos: DatosService, private router: Router,
     private ar: ActivatedRoute, private modalService: NgbModal) { }
 
 
   ngOnInit(): void {
+
     this.obtener();
+    setTimeout(() => this.staticAlertClosed = true, 20000);
+
+    this._success.subscribe(message => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = '');
   }
 
   obtener()
@@ -38,8 +51,29 @@ export class AddpedidoComponent implements OnInit {
   }
   mandar(form)
   {
-    console.log(form.value,"d")
+    this.porductoS=form._directives.map(item=> (
+      {
+      "nombre":item.name,
+      "valor":item.value}
+      ) )
+    console.log(this.porductoS)
+   /*for(let i in form._directives)
+    if(form._directives[i].value)
+      this.productos.push( {form._directives[i].name : form._directives[i].value});
+*/
+  }
 
+  close(alertt) {
+    this.alert.splice(this.alert.indexOf(alertt), 1);
+  }
+
+
+  staticAlertClosed = false;
+  successMessage = '';
+
+
+  public changeSuccessMessage() {
+    this._success.next(` Datos incompeltos`);
   }
 
 }
